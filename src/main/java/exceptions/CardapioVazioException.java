38a0622fb6d3c6e3651f96cbf4b2f504cc26994e
Rafael Blumm <rafaelfloresblumm@gmail.com>
2023-06-04 16:55:49 -0300

    Eliminação de método e variável redundantes

--- src/main/java/exceptions/CardapioVazioException.java
+++ src/main/java/exceptions/CardapioVazioException.java
@@ -4,14 +4,7 @@ package exceptions;
  * Sinaliza que o cardápio informado tem registro vazio.
  */
 public class CardapioVazioException extends Throwable {
-    private final String mensagem;
-
     public CardapioVazioException(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    @Override
-    public String getMessage() {
-        return mensagem;
+        super(mensagem);
     }
 }
